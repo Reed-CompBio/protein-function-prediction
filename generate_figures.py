cb5ddb84412039d0ab469b4f47cc92966623a182
ctrlaltaf <altafayyubibarelvi@gmail.com>
2024-09-09 13:39:16 -0700

    changing data structure

--- generate_figures.py
+++ generate_figures.py
@@ -26,36 +26,79 @@ def read_file(filepath: str) -> dict:
 def get_roc_data(data_df: dict) -> list:
     y = np.array(data_df["y_true"])
     scores = np.array(data_df["y_score"])
-    fpr, tpr, threshold = roc_curve(
-        y, scores, pos_label=1
-    )
+    fpr, tpr, threshold = roc_curve(y, scores, pos_label=1)
     roc_auc = auc(fpr, tpr)
 
     return fpr, tpr, threshold, roc_auc
 
-# def create_plot()
+
+def create_plot(x_data: list, y_data: list, auc: float, type: str) -> None:
+    plt.plot(
+        x_data,
+        y_data,
+        color="red",
+        lw=2,
+        label=f"{type}(area = %0.2f)" % auc,
+    )
 
 
 def main():
     print("Generating figures")
     # read in the files and get the score and label
-    testing = Path("./results/overlapping_neighbor_data.csv")
+    overlapping_path = Path(
+        "./results/final-inferred-complete/elegans/overlapping_neighbor_data.csv"
+    )
+    hypergeometric_path = Path(
+        "./results/final-inferred-complete/elegans/hypergeometric_distribution.csv"
+    )
+    degree_path = Path(
+        "./results/final-inferred-complete/elegans/protein_degree_v3_data.csv"
+    )
+    rw_path = Path("./results/final-inferred-complete/elegans/random_walk_data_v2.csv")
 
-    testing_df = read_file(testing)
-    results_df = {}
+    overlapping_df = read_file(overlapping_path)
+    hypergeometric_df = read_file(hypergeometric_path)
+    degree_df = read_file(degree_path)
+    rw_df = read_file(rw_path)
 
     # calculate AUC values
-    fpr, tpr, threshold, roc_auc = get_roc_data(testing_df)
-    print(fpr, tpr, threshold, roc_auc)
-    # plot the images
+    fpr1, tpr1, threshold1, roc_auc1 = get_roc_data(overlapping_df)
+    fpr2, tpr2, threshold2, roc_auc2 = get_roc_data(hypergeometric_df)
+    fpr3, tpr3, threshold3, roc_auc3 = get_roc_data(degree_df)
+    fpr4, tpr4, threshold4, roc_auc4 = get_roc_data(rw_df)
 
-    plt.plot(
-        fpr,
-        tpr,
-        color="red",
-        lw=2,
-        label=f"(area = %0.2f)" % roc_auc,
+    elegans = {
+        "fpr": [fpr1, fpr2, fpr3, fpr4],
+        "tpr": [tpr1, tpr2, tpr3, tpr4],
+        "roc": [roc_auc1, roc_auc2, roc_auc3, roc_auc4],
+        "method": ["Overlapping", "Hypergeometric", "Degree", "RW"],
+    }
+    final_data = {"elegans": elegans}
+
+    # # plot the images
+    fig_width = 10  # width in inches
+    fig_height = 7  # height in inches
+    fig_dpi = 100  # dots per inch for the figure
+    save_dpi = 200  # dots per inch for the saved image
+    plt.figure(figsize=(fig_width, fig_height), dpi=fig_dpi)
+
+    create_plot(
+        final_data["elegans"]["fpr"][0],
+        final_data["elegans"]["tpr"][0],
+        final_data["elegans"]["roc"][0],
+        final_data["elegans"]["method"][0],
     )
+    create_plot(
+        final_data["elegans"]["fpr"][0],
+        final_data["elegans"]["tpr"][0],
+        final_data["elegans"]["roc"][0],
+        final_data["elegans"]["method"][0],
+    )
+    
+    # create_plot(fpr2, tpr2, roc_auc2, "hypergeometric")
+    # create_plot(fpr3, tpr3, roc_auc3, "degree")
+    # create_plot(fpr4, tpr4, roc_auc4, "rw")
+
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel("False Positive Rate")
